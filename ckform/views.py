367292d07a1844f89867c3606f8778add4b6ff9d
Franklin Antony <franklinanton.007@gmail.com>
2018-06-27 16:44:48 +0530

    Text POST submisson working

--- ckform/views.py
+++ ckform/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 # Create your views here.
 from django.http import HttpResponse
 from .forms import ArticleForm
 
 def index(request):
-	context = {'form': ArticleForm()}
-	return render(request,'index.html',context)
+	if request.method == 'POST':
+		form = ArticleForm(request.POST)
+		if form.is_valid():
+			form.save()
+			context = {'form': form}
+			return redirect('index')
+		else:
+			context = {'form': ArticleForm()}
+			return render(request, 'index.html', context)
+	else:
+		context = {'form': ArticleForm()}
+		return render(request,'index.html',context)
