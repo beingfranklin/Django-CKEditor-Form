299487882b3e3fd83d701a7446c2b55f380db9d0
Franklin Antony <franklinanton.007@gmail.com>
2018-06-27 14:44:03 +0530

    imported models.py, views.py, and templates folder

---
+++ ckform/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CkformConfig(AppConfig):
+    name = 'ckform'
---
+++ ckform/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+# Create your views here.
+from django.http import HttpResponse
+from .forms import ArticleForm
+
+def index(request):
+	context = {'form': ArticleForm()}
+	return render(request,'index.html',context)
